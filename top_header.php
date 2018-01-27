4860a6e3f8d06326d22c356146d9d30e6f6ff481
cobolmx <irodrigu@gmail.com>
2018-01-26 21:46:33 -0800

    first update
    first update

---
+++ top_header.php
@@ -0,0 +1,21 @@
+<div class="top-header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-10">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-clock-o"></i> Lunes - Viernes: 8am - 5pm | Sábados 8:00 am  - 2:00 pm</a></li>
+                            <li><a href="#"><i class="fa fa-envelope"></i> info@72mechanix.com</a></li>
+                            <li><a href="#"><i class="fa fa-phone"></i> (686) 290-6974, 290-6977</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-12 col-md-2">
+                        <div class="share-icons">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="https://www.facebook.com/7.2mechanix/"><i class="fa fa-facebook"></i></a></li>                                
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
