a374b0ac48c902b5171874c33ea9fdf28bcd2a35
cobolmx <irodrigu@gmail.com>
2018-01-27 17:47:04 -0800

    update sabado
    faltan fotos y algunas cosas de contacto y mapa

--- index.php
+++ index.php
@@ -376,13 +376,13 @@
                             </div>
                             <div class="row">
                                 <div class="col-md-6">
-                                    <label>Appointment Date</label>
-                                    <input type="text" id="datepicker" name="appointmentDate" placeholder="Appointment Date" required>
+                                    <label>Fecha de cita</label>
+                                    <input type="text" id="datepicker" name="appointmentDate" placeholder="Fecha para la cita" required>
                                 </div>
                                 <div class="col-md-6">
-                                    <label>Appointment Time</label>
+                                    <label>Tiempo para la cita</label>
                                     <select id="Time">
-                                        <option value="hide">-- Choose --</option>
+                                        <option value="hide">-- Elija --</option>
                                         <option value="2010">09:00AM - 10:00AM</option>
                                         <option value="2011">10:00AM - 11:00AM</option>
                                         <option value="2012">11:00AM - 12:00PM</option>
@@ -400,28 +400,28 @@
                             </div>
                             <div class="row">
                                <div class="col-md-12">
-                                   <p>Contact Details</p>
+                                   <p>Detalles del contacto</p>
                                </div>
                                 <div class="col-md-6">
-                                    <input type="text" name="yourName" placeholder="Your Name" required>
+                                    <input type="text" name="yourName" placeholder="Nombre" required>
                                 </div>
                                 <div class="col-md-6">
-                                    <input type="email" name="yourEmail" placeholder="Your Email" required>
+                                    <input type="email" name="yourEmail" placeholder="Correo eléctronico" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
-                                    <input type="text" name="yourPhone" placeholder="Your Phone" required>
+                                    <input type="text" name="yourPhone" placeholder="Télefono" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12">
-                                    <textarea name="yourMessage" id="yourMessage" placeholder="Your Message" required></textarea>
+                                    <textarea name="yourMessage" id="yourMessage" placeholder="Mensaje" required></textarea>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 text-center">
-                                    <button class="btn theme-btn" type="submit">Submit Now</button>
+                                    <button class="btn theme-btn" type="submit">Enviar</button>
                                 </div>
                             </div>
                         </form>
@@ -432,7 +432,7 @@
         <!-- End Appointment Area -->
         
         <!-- Start News Area -->
-        <div class="content-block-area gray-bg">
+        <!-- <div class="content-block-area gray-bg">
             <div class="container">
                <div class="row">
                    <div class="col-md-6 col-md-offset-3">
@@ -523,7 +523,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- End News Area -->
         
         <!-- Start Our Parners Area -->
@@ -532,9 +532,9 @@
                <div class="row">
                    <div class="col-md-6 col-md-offset-3">
                        <div class="section-title text-center">
-                           <h2><span>Our</span> Partner</h2>
+                           <h2><span>Nuestros</span> Clientes</h2>
                            <div class="car-icon"><img src="assets/img/cars.png" alt="car"></div>
-                           <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                           <p>Aqui va el barrido de nuestros clientes</p>
                        </div>
                    </div>
                 </div>
@@ -570,17 +570,17 @@
                    <div class="row">
                        <div class="col-sm-4">
                             <div class="footer-top-info">
-                               <span class="footer-icons"><i class="fa fa-map-o"></i></span> <p>1828 Johns Drive Glenview, IL 60025</p>
+                               <span class="footer-icons"><i class="fa fa-map-o"></i></span> <p>Blvd. Lázaro Cárdenas #615 Fracc. Jardines del lago</p>
                             </div>
                        </div>
                        <div class="col-sm-4">
                             <div class="footer-top-info">
-                               <span class="footer-icons"><i class="flaticon-crane"></i></span> <p>Working Days Monday to Friday</p>
+                               <span class="footer-icons"><i class="flaticon-crane"></i></span> <p>Lunes a Sábado</p>
                             </div>
                        </div>
                        <div class="col-sm-4">
                             <div class="footer-top-info">
-                               <span class="footer-icons"><i class="fa fa-headphones"></i></span> <p>Give us a free call (224) 228-8475</p>
+                               <span class="footer-icons"><i class="fa fa-headphones"></i></span> <p>(686) 290-6974</p><p> (686) 290-6977</p>
                             </div>
                        </div>
                    </div>
@@ -588,7 +588,7 @@
                     <div class="row">
                         <div class="col-md-4 col-sm-12">
                             <div class="footer-wid">
-                                <a href="index.html" class="footer-logo"><img src="assets/img/logo.png" alt="logo"></a>
+                                <a href="index.html" class="footer-logo"><img src="img_logo/logo.png" alt="logo"></a>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                                 <a href="#" class="link-color">Read More About Us <i class="fa  fa-long-arrow-right"></i></a>
                             </div>
@@ -597,15 +597,15 @@
                             <div class="row">
                                 <div class="col-md-4 col-sm-4">
                                     <div class="footer-wid footer-menu">
-                                        <h3 class="footer-wid-title">Navigation</h3>
+                                        <h3 class="footer-wid-title">Navegación</h3>
                                         <ul>
-                                            <li><a href="index.html"><i class="fa  fa-angle-right"></i> home</a></li>
-                                            <li><a href="about-us.html"><i class="fa  fa-angle-right"></i> About</a></li>
+                                            <li><a href="index.php"><i class="fa  fa-angle-right"></i> Inicio</a></li>
+                                            <!-- <li><a href="about-us.html"><i class="fa  fa-angle-right"></i> About</a></li>
                                             <li><a href="projects.html"><i class="fa  fa-angle-right"></i> our mission</a></li>
                                             <li><a href="industries.html"><i class="fa  fa-angle-right"></i> services</a></li>
                                             <li><a href="gallery.html"><i class="fa  fa-angle-right"></i> gallery</a></li>
                                             <li><a href="blog.html"><i class="fa  fa-angle-right"></i> blog</a></li>
-                                            <li><a href="contact-us.html"><i class="fa  fa-angle-right"></i> contact</a></li>
+                                            <li><a href="contact-us.html"><i class="fa  fa-angle-right"></i> contact</a></li> -->
                                         </ul>
                                     </div>
                                 </div>
@@ -614,29 +614,30 @@
                                     <div class="footer-wid footer-menu">
                                         <h3 class="footer-wid-title">Hours of Operation</h3>
                                         <ul>
-                                            <li><i class="fa  fa-angle-right"></i> Monday:7:30am - 5:30pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Tuesday:7:30am - 5:30pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Wednesday:7:30am - 5:30pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Thursday:7:30am - 5:30pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Friday:7:30am - 5:30pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Saturday:7:30am - 3:00pm</li>
-                                            <li><i class="fa  fa-angle-right"></i> Sunday: Closed</li>
+                                            <li><i class="fa  fa-angle-right"></i> Lunes:8:00am - 5:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Martes:8:00am - 5:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Miercoles:8:00am - 5:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Jueves:8:00am - 5:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Viernes:8:00am - 5:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Sábado:8:00am - 2:00pm</li>
+                                            <li><i class="fa  fa-angle-right"></i> Domingo: Cerrado</li>
                                         </ul>
                                     </div>
                                 </div>
 
                                 <div class="col-md-4 col-sm-4">
                                     <div class="footer-wid">
-                                        <h3 class="footer-wid-title">Get In Touch</h3>
-                                        <p>1828 Johns Drive Glenview, IL 60025</p>
+                                        <h3 class="footer-wid-title">Contacto</h3>
+                                        <p>Blvd. Lázaro Cárdenas #615 Fracc. Jardines del Lago</p>
                                         <div class="address-info">
-                                            <span><i class="fa  fa-phone"></i> (224) 228-8475</span><br>
-                                            <span><i class="fa  fa-envelope"></i> support@carservice.com </span>
+                                            <span><i class="fa  fa-phone"></i> (686) 290-6974</span><br>
+                                            <span><i class="fa  fa-phone"></i> (686) 290-6977</span><br>
+                                            <span><i class="fa  fa-envelope"></i> info@72mechanix.com </span>
                                         </div>
                                         <div class="subscribe">
                                             <form action="index.html">
                                                 <input type="text" placeholder="Type your email" required>
-                                                <button>Subscribe now <i class="fa  fa-paper-plane"></i></button>
+                                                <button>Subscribirse <i class="fa  fa-paper-plane"></i></button>
                                             </form>
                                         </div>
                                     </div>
@@ -651,9 +652,9 @@
                                 <ul class="list-inline">
                                     <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                                     <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                    <!-- <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                                     <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li> -->
                                 </ul>
                             </div>
                         </div>
@@ -669,10 +670,10 @@
                             &copy; Copyright <script type="text/javascript">
                             var d=new Date();
                             document.write(d.getFullYear());
-                            </script> All Rights Reserved.
+                            </script> Todos los derechos reservados.
                         </div>
                         <div class="col-md-6 col-sm-7 text-right">
-                            <a href="#">Terms & Condition</a> <span class="seprator">|</span> <a href="#">Privacy Policy</a>
+                            <a href="#">Terminos & Condiciones</a> <span class="seprator">|</span> <a href="#">Politica de privacidad</a>
                         </div>
                     </div>
                 </div>
@@ -680,7 +681,7 @@
         </footer> <!-- End Footer Area -->
         
         <!-- Back Top top -->
-        <a href="#content" class="back-to-top">Top</a>
+        <a href="#content" class="back-to-top">Arriba</a>
         <!-- End Back Top top -->
         
 		<!-- Bootstrap JS file -->
